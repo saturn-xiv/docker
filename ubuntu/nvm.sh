dc5dac9a21ea8bd5971b6ff8631f0a155ecd0860
Jeremy Zheng <jitang.zheng@gmail.com>
2018-11-02 17:56:09 -0700

    remove shell scripts

--- ubuntu/nvm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | sh
-echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.zshrc
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $HOME/.zshrc
-
-source $HOME/.zshrc || true
-
-nvm install node
